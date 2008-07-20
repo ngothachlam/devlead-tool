8a6ab492df46c35ea1028fd780523e61fac41c45
pjw.olofsson <pjw.olofsson@9ed4f39a-2752-0410-b9c2-9f4538f363dd>
2008-07-20 22:20:02 +0000

    Jonas: Added some unit tests. Need to add checkbox renderer and continue on excel funtional e2e test

--- trunk/test-unit/com/jonas/agile/devleadtool/component/table/renderer/CheckBoxTableCellRendererTest.java
+++ trunk/test-unit/com/jonas/agile/devleadtool/component/table/renderer/CheckBoxTableCellRendererTest.java
@@ -3,23 +3,24 @@ package com.jonas.agile.devleadtool.component.table.renderer;
 import java.awt.Color;
 
 import javax.swing.JTable;
-import javax.swing.event.TableModelListener;
-
-import com.jonas.agile.devleadtool.component.table.model.MyTableModel;
 
 import junit.framework.TestCase;
 
+import org.easymock.EasyMock;
+
+import com.jonas.agile.devleadtool.component.table.model.MyTableModel;
+
 public class CheckBoxTableCellRendererTest extends TestCase {
 
-	CheckBoxTableCellRenderer renderer_errors;
-
-	CheckBoxTableCellRenderer renderer_ok;
+	CheckBoxTableCellRenderer renderer;
+	MyTableModel myTableModel_Mock;
 
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-		renderer_errors = new CheckBoxTableCellRenderer(new TestTableModel(true));
-		renderer_ok = new CheckBoxTableCellRenderer(new TestTableModel(false));
+		myTableModel_Mock = EasyMock.createMock(MyTableModel.class);
+		renderer = new CheckBoxTableCellRenderer(myTableModel_Mock);
+
 	}
 
 	@Override
@@ -29,60 +30,15 @@ public class CheckBoxTableCellRendererTest extends TestCase {
 
 	public void testShouldSetCorrectColor() {
 		// TODO implement easyMock!!!
-		assertEquals(Color.red, renderer_errors.getTableCellRendererComponent(new JTable(), Boolean.TRUE, true, true, 0, 0).getBackground());
+
+		EasyMock.expect(myTableModel_Mock.shouldBackgroundBeRed(true, 0, 0)).andReturn(false);
+
+		EasyMock.replay(myTableModel_Mock);
+
+		assertEquals(Color.red, renderer.getTableCellRendererComponent(new JTable(), Boolean.TRUE, true, true, 0, 0)
+				.getBackground());
+
+		EasyMock.verify(myTableModel_Mock);
+
 	}
-
-	private final class TestTableModel implements MyTableModel {
-
-		boolean shouldBeRed = false;
-
-		public void addEmptyRow() {
-		}
-
-		public void removeSelectedRows(JTable table) {
-		}
-
-		public boolean shouldBackgroundBeRed(Object value, int row, int column) {
-			return shouldBeRed;
-		}
-
-		public void addTableModelListener(TableModelListener arg0) {
-		}
-
-		public Class<?> getColumnClass(int arg0) {
-			return null;
-		}
-
-		public int getColumnCount() {
-			return 0;
-		}
-
-		public String getColumnName(int arg0) {
-			return null;
-		}
-
-		public int getRowCount() {
-			return 0;
-		}
-
-		public Object getValueAt(int arg0, int arg1) {
-			return null;
-		}
-
-		public boolean isCellEditable(int arg0, int arg1) {
-			return false;
-		}
-
-		public void removeTableModelListener(TableModelListener arg0) {
-		}
-
-		public void setValueAt(Object arg0, int arg1, int arg2) {
-		}
-
-		public TestTableModel(boolean shouldBeRed) {
-			super();
-			this.shouldBeRed = shouldBeRed;
-		}
-	}
-
 }
