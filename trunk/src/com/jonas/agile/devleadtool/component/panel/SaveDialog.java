0a26be0ac700c2341c8e5cde93785da9410c2acb
pjw.olofsson <pjw.olofsson@9ed4f39a-2752-0410-b9c2-9f4538f363dd>
2008-08-07 17:16:57 +0000

    Jonas: small change

--- trunk/src/com/jonas/agile/devleadtool/component/panel/SaveDialog.java
+++ trunk/src/com/jonas/agile/devleadtool/component/panel/SaveDialog.java
@@ -2,25 +2,39 @@ package com.jonas.agile.devleadtool.component.panel;
 
 import java.awt.Component;
 import java.io.File;
-import java.io.IOException;
+
 import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
+
 import com.jonas.agile.devleadtool.PlannerHelper;
 import com.jonas.agile.devleadtool.data.PlannerDAO;
 
 public class SaveDialog extends JFileChooser {
 
-   public SaveDialog(PlannerDAO dao, Component frame, PlannerHelper plannerHelper) {
-      super(new File("."));
-      File file = plannerHelper.getFile();
-      if (file != null)
-         setSelectedFile(file);
+	public SaveDialog(PlannerDAO dao, Component frame, PlannerHelper plannerHelper) {
+		super(new File("."));
+		File file = plannerHelper.getFile();
+		if (file != null)
+			setSelectedFile(file);
+		
+		// addChoosableFileFilter(new FileFilter() {
+		// public boolean accept(File f) {
+		// if (getTypeDescription(f).equalsIgnoreCase("Microsoft Excel Worksheet") || f.isDirectory())
+		// return true;
+		// return false;
+		// }
+		//
+		// public String getDescription() {
+		// return "XLS files";
+		// }
+		// });
 
-      int result = showSaveDialog(frame);
-      if (result == JFileChooser.APPROVE_OPTION) {
-         File selFile = getSelectedFile();
-         plannerHelper.setFile(selFile);
-         plannerHelper.saveModels(selFile, dao);
-      }
-   }
+		int result = showSaveDialog(frame);
+		if (result == JFileChooser.APPROVE_OPTION) {
+			File selFile = getSelectedFile();
+			plannerHelper.setFile(selFile);
+			plannerHelper.saveModels(selFile, dao);
+		}
+	}
 
 }
