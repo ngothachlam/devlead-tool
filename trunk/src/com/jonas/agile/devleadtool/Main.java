20aa70b02fcd914ab4fad375c70c34427816428c
pjw.olofsson <pjw.olofsson@9ed4f39a-2752-0410-b9c2-9f4538f363dd>
2008-08-09 12:07:58 +0000

    Jonas: put something else on the TODO list

--- trunk/src/com/jonas/agile/devleadtool/Main.java
+++ trunk/src/com/jonas/agile/devleadtool/Main.java
@@ -13,13 +13,13 @@ public class Main {
 		}
 	}
 
+	// TODO in boardPanel create the HyperLinker within the JiraIssue or make something nicer so you can set "defaultCellRenderer(HyperLink.class, blah)"
 	// TODO handle 'issue not found' when syncing to jira and getting info (LLU-2)
 	// TODO handle issue moved to other project (LLU-6 that has become LLUOLD-1)
 	// TODO Add project textbox for default jiraprefix when adding to board
 	// TODO add lock mechanism to unable checkbox ticking, etc to prevent accidental changes
 	// TODO add checkboxes to renderer
-	// TODO add other colors and make this OO - Jira status vs. Status code (inprogress should be mapped to blue (focus, selected,
-	// editable)
+	// TODO add other colors and make this OO - Jira status vs. Status code (inprogress should be mapped to blue (focus, selected, editable)
 	// TODO use enums to generic handle table model updates and set correct color?
 	// TODO add a xml file to save what save location for each planner opened.
 	// TODO add system properties to store last save and load location directory
