3f11ec7490d7b920d0b557fd0a052dc2421b1f91
pjw.olofsson <pjw.olofsson@9ed4f39a-2752-0410-b9c2-9f4538f363dd>
2008-11-02 00:25:43 +0000

    performance improvements

--- src/com/jonas/agile/devleadtool/component/dialog/NewPlannerDialog.java
+++ src/com/jonas/agile/devleadtool/component/dialog/NewPlannerDialog.java
@@ -4,25 +4,40 @@ import javax.swing.SwingWorker;
 import com.jonas.agile.devleadtool.PlannerHelper;
 import com.jonas.agile.devleadtool.component.MyDesktopPane;
 import com.jonas.agile.devleadtool.component.MyInternalFrame;
+import com.jonas.agile.devleadtool.component.SaveKeyListener;
 import com.jonas.agile.devleadtool.component.panel.InternalTabPanel;
 import com.jonas.agile.devleadtool.component.table.model.BoardTableModel;
 import com.jonas.agile.devleadtool.component.table.model.JiraTableModel;
 import com.jonas.agile.devleadtool.data.PlannerDAOExcelImpl;
 
 public class NewPlannerDialog {
-   public NewPlannerDialog(final MyDesktopPane desktopPane, final PlannerHelper helper, final PlannerDAOExcelImpl dao, final SavePlannerDialog savePlannerDialog) {
+   private final MyDesktopPane desktopPane;
+   private final PlannerHelper helper;
+   private final PlannerDAOExcelImpl dao;
+   private final SavePlannerDialog savePlannerDialog;
+   private final SaveKeyListener saveKeyListener;
+
+   public NewPlannerDialog(final MyDesktopPane desktopPane, final PlannerHelper helper, final PlannerDAOExcelImpl dao, final SavePlannerDialog savePlannerDialog, final SaveKeyListener saveKeyListener) {
+      this.desktopPane = desktopPane;
+      this.helper = helper;
+      this.dao = dao;
+      this.savePlannerDialog = savePlannerDialog;
+      this.saveKeyListener = saveKeyListener;
       
+   }
+   
+   public void openNew(){
       SwingWorker<ModelDTO, Object> worker = new SwingWorker<ModelDTO, Object>() {
          protected ModelDTO doInBackground() throws Exception {
             return new ModelDTO(new BoardTableModel(), new JiraTableModel());
          }
-
+         
          @Override
          protected void done() {
             try {
                ModelDTO dto = get();
                InternalTabPanel internalFrameTabPanel = new InternalTabPanel(helper, dto.getBoardModel(), dto.getJiraModel());
-               MyInternalFrame internalFrame = new MyInternalFrame(helper, helper.getTitle(), internalFrameTabPanel, dao, savePlannerDialog);
+               MyInternalFrame internalFrame = new MyInternalFrame(helper, helper.getTitle(), internalFrameTabPanel, dao, savePlannerDialog, saveKeyListener);
                desktopPane.addInternalFrame(internalFrame);
                internalFrame.setVisible(true);
             } catch (Throwable e) {
@@ -30,9 +45,9 @@ public class NewPlannerDialog {
                e.printStackTrace();
             }
          }
-
-
-
+         
+         
+         
       };
       worker.execute();
    }
