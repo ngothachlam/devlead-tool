3362f1aa115521fbee3695a8d7da266553d6b3b4
pjw.olofsson <pjw.olofsson@9ed4f39a-2752-0410-b9c2-9f4538f363dd>
2009-02-18 21:07:14 +0000

    Updated the parseHTML to get out rollforwards!

--- trunk/src/com/jonas/rollforwardapp/RollforwardParser.java
+++ trunk/src/com/jonas/rollforwardapp/RollforwardParser.java
@@ -1,13 +1,26 @@
 package com.jonas.rollforwardapp;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RollforwardParser {
 
-   public String[] parseJiraHTMLAndGetSqlRollForwards(String html) {
-      throw new RuntimeException("Method not implemented yet!");
-   }
+   private Pattern pattern = Pattern.compile("http://your.host.address/viewcvs.cgi/(.*)/\\?");
 
-   public String parseLineOfHTML(String string) {
-      throw new RuntimeException("Method not implemented yet!");
+   public String[] parseJiraHTMLAndGetSqlRollForwards(String inputStr) {
+      Matcher matcher = pattern.matcher(inputStr);
+
+      boolean matchFound = matcher.find();
+      if (matchFound) {
+         int groupCount = matcher.groupCount();
+         String[] result = new String[groupCount];
+         for (int i = 0; i <= groupCount-1; i++) {
+            result[i] = matcher.group(i+1);
+         }
+         return result;
+      }
+      return null;
+
    }
 
 }
