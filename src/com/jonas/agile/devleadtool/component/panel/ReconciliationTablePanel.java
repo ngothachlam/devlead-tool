cf876481be09284037a6e9ab0afa91a0c7ef7034
pjw.olofsson <pjw.olofsson@9ed4f39a-2752-0410-b9c2-9f4538f363dd>
2009-03-17 16:57:59 +0000

    Separating out MyTable into MyAbstractTable

--- src/com/jonas/agile/devleadtool/component/panel/ReconciliationTablePanel.java
+++ src/com/jonas/agile/devleadtool/component/panel/ReconciliationTablePanel.java
@@ -1,14 +1,16 @@
 package com.jonas.agile.devleadtool.component.panel;
 
 import java.awt.BorderLayout;
+import javax.swing.JTable;
 import com.jonas.common.MyPanel;
 
 public class ReconciliationTablePanel extends MyPanel {
 
+   private JTable table;
+
    public ReconciliationTablePanel() {
       super(new BorderLayout());
-      
-      
+      table = new JTable();
    }
 
 }
