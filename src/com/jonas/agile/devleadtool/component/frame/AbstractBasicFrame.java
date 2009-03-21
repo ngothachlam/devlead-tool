080afa96b6172ecb6e54ee80fd68bc4c313b5448
pjw.olofsson <pjw.olofsson@9ed4f39a-2752-0410-b9c2-9f4538f363dd>
2009-03-21 16:45:33 +0000

    Jonas: Adding boardstats panel burndown - first step - still ugly!

--- src/com/jonas/agile/devleadtool/component/frame/AbstractBasicFrame.java
+++ src/com/jonas/agile/devleadtool/component/frame/AbstractBasicFrame.java
@@ -6,14 +6,27 @@ import javax.swing.JFrame;
 import com.jonas.common.SwingUtil;
 
 public abstract class AbstractBasicFrame extends JFrame {
-   
-   public AbstractBasicFrame(Component parent, int width, int height) {
-      this.setContentPane(getMyPanel());
-      this.pack();
-      this.setSize(width, height);
 
-      SwingUtil.centreWindowWithinWindow(this, parent);
-      setVisible(true);
+   private final Component parent;
+   private final int width;
+   private final int height;
+
+   public AbstractBasicFrame(Component parent, int width, int height) {
+      this.parent = parent;
+      this.width = width;
+      this.height = height;
+   }
+
+   public final void setVisible(boolean isVisible) {
+      if (isVisible) {
+         this.setContentPane(getMyPanel());
+         this.pack();
+         this.setSize(width, height);
+
+         if (parent != null)
+            SwingUtil.centreWindowWithinWindow(this, parent);
+      }
+      super.setVisible(isVisible);
    }
 
    public abstract Container getMyPanel();
--- src/com/jonas/agile/devleadtool/component/frame/BoardStatsFrame.java
+++ src/com/jonas/agile/devleadtool/component/frame/BoardStatsFrame.java
@@ -2,17 +2,62 @@ package com.jonas.agile.devleadtool.component.frame;
 
 import java.awt.Component;
 import java.awt.Container;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
 public class BoardStatsFrame extends AbstractBasicFrame{
 
+   private ChartPanel panel;
+
    public BoardStatsFrame(Component parent, int width, int height) {
       super(parent, width, height);
+      
+      calculateData();
+   }
+
+   private void calculateData() {
+      XYSeries series1 = new XYSeries("Remaining Dev Estimates");
+      series1.add(0, 20);
+      series1.add(1, 19);
+      series1.add(2, 16);
+
+      XYSeriesCollection dataset = new XYSeriesCollection();
+      dataset.addSeries(series1);
+
+      // create a chart...
+      // create the chart...
+      JFreeChart chart = ChartFactory.createXYLineChart("Burndown of Current Sprint", // chart title
+            "Day in Sprint", // x axis label
+            "Points", // y axis label
+            dataset, // data
+            PlotOrientation.VERTICAL, false, // include legend
+            true, // tooltips
+            false // urls
+            );
+
+      XYPlot plot = chart.getXYPlot();
+      ValueAxis domainAxis = plot.getDomainAxis();
+      domainAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+      domainAxis.setLowerBound(0);
+      domainAxis.setUpperBound(10);
+
+      // change the auto tick unit selection to integer units only...
+      NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+      rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+
+      panel = new ChartPanel(chart);
    }
 
    @Override
    public Container getMyPanel() {
-      // TODO Auto-generated method stub
-      throw new RuntimeException("Method not implemented yet!");
+      return panel;
    }
 
 }
