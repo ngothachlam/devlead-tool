f0344e788d05157bdcb9fd49b72897472d8d399d
pjw.olofsson <pjw.olofsson@9ed4f39a-2752-0410-b9c2-9f4538f363dd>
2009-04-17 15:16:28 +0000

    Jonas: TreeTableRefactor

--- src/com/jonas/common/logging/MyLogger.java
+++ src/com/jonas/common/logging/MyLogger.java
@@ -1,8 +1,6 @@
 package com.jonas.common.logging;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
@@ -12,7 +10,6 @@ public class MyLogger {
    private static String log4jPropertyLocation = "properties/log4j.properties";
 
    private static boolean setup = false;
-   private final static Map<Class<?>, Logger> logs = new HashMap<Class<?>, Logger>();
 
    public static Logger getLogger(Class className) {
       if (!setup) {
@@ -35,18 +32,5 @@ public class MyLogger {
       System.out.println("Setting up log4j property location \"" + log4jPropertyLoc + "\" and it is " + (!exists ? "NOT " : "") + "a file!");
       BasicConfigurator.configure();
       PropertyConfigurator.configure(log4jPropertyLoc);
-   }
-
-   public static void warn(Class<?> class1, String string) {
-      Logger logger = logs.get(class1);
-      if (logger == null) {
-         synchronized (logs) {
-            if (!logs.containsKey(class1)) {
-               logger = getLogger(class1);
-               logs.put(class1, logger);
-            }
-         }
-      }
-      logger.warn(string);
    }
 }
