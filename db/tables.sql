e9260c33721f23af42ee5009a34eddd10fd89478
pjw.olofsson <pjw.olofsson@9ed4f39a-2752-0410-b9c2-9f4538f363dd>
2009-04-18 15:16:35 +0000

    Jonas: db stuff

--- db/tables.sql
+++ db/tables.sql
@@ -1,7 +1,7 @@
 -- ============================
 
 -- This file was created using Derby's dblook utility.
--- Timestamp: 2009-04-18 16:05:06.64
+-- Timestamp: 2009-04-18 16:15:35.0
 -- Source database is: db/data/test
 -- Connection URL is: jdbc:derby:db/data/test;create=true
 -- appendLogs: false
