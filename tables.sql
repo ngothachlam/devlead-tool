1fd7e791b3dc2456d09140c5bbd5ce776d60564a
pjw.olofsson <pjw.olofsson@9ed4f39a-2752-0410-b9c2-9f4538f363dd>
2009-04-18 14:14:43 +0000

    Jonas: db stuff

--- tables.sql
+++ tables.sql
@@ -1,8 +1,14 @@
 -- ============================
 
 -- This file was created using Derby's dblook utility.
--- Timestamp: 2009-04-18 14:42:01.531
+-- Timestamp: 2009-04-18 14:53:41.703
 -- Source database is: db/data/test
 -- Connection URL is: jdbc:derby:db/data/test;create=true
 -- appendLogs: false
+
+-- ----------------------------------------------
+-- DDL Statements for tables
+-- ----------------------------------------------
+
+CREATE TABLE "APP"."TEST" ("ID" INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), "NAME" CHAR(10));
 
