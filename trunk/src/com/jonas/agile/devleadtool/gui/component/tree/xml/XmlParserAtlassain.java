2f275c8e84849278801cf3158d6385e0d1dcd012
pjw.olofsson <pjw.olofsson@9ed4f39a-2752-0410-b9c2-9f4538f363dd>
2009-04-21 17:32:06 +0000

    Jonas: Refactoring to enable me to load fix versions prior to syncing

--- trunk/src/com/jonas/agile/devleadtool/gui/component/tree/xml/XmlParserAtlassain.java
+++ trunk/src/com/jonas/agile/devleadtool/gui/component/tree/xml/XmlParserAtlassain.java
@@ -3,19 +3,14 @@ package com.jonas.agile.devleadtool.gui.component.tree.xml;
 import java.io.IOException;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
-import org.apache.log4j.Logger;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.XMLReaderFactory;
-import com.jonas.common.logging.MyLogger;
 import com.jonas.jira.JiraProject;
 import com.jonas.jira.access.JiraException;
 
 public class XmlParserAtlassain extends HttpClient implements XmlParser {
 
-   private Logger log = MyLogger.getLogger(XmlParserAtlassain.class);
-
-   private static final String MAX_RESULTS = "25";
    private XMLReader reader;
 
    public XmlParserAtlassain(JiraSaxHandler nodeCounter) throws SAXException {
