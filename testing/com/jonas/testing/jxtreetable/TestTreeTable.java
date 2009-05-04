ab8cb97ea10d8446f6781ddbf936e954d7b2be3b
pjw.olofsson <pjw.olofsson@9ed4f39a-2752-0410-b9c2-9f4538f363dd>
2009-05-04 13:51:13 +0000

    Jonas: Refactoring slightly

--- testing/com/jonas/testing/jxtreetable/TestTreeTable.java
+++ testing/com/jonas/testing/jxtreetable/TestTreeTable.java
@@ -3,13 +3,14 @@ package com.jonas.testing.jxtreetable;
 import java.awt.Component;
 import java.io.File;
 import java.io.IOException;
+
 import javax.swing.JScrollPane;
-import javax.swing.JTree;
 import javax.swing.UIManager;
-import javax.swing.tree.TreeCellRenderer;
+
 import org.jdesktop.swingx.JXTreeTable;
 import org.jdesktop.swingx.treetable.DefaultMutableTreeTableNode;
 import org.jdesktop.swingx.treetable.DefaultTreeTableModel;
+
 import com.jonas.testHelpers.TryoutTester;
 import com.jonas.testing.jxtreetable.column.BoardColumnMapperImpl;
 import com.jonas.testing.jxtreetable.dao.DaoTreeBuilder;
@@ -45,22 +46,7 @@ public class TestTreeTable {
     * 
     */
    public void create() throws IOException {
-      SprintUserObject sprint_12_1 = new SprintUserObject("12-1");
-      SprintUserObject sprint_12_2 = new SprintUserObject("12-2");
-      FixVersionUserObject fixVersion_11 = new FixVersionUserObject("LLU 11");
-      FixVersionUserObject fixVersion_12 = new FixVersionUserObject("LLU 12");
-      JiraUserObject jira_llu1 = new JiraUserObject("llu-123", "Description for llu-1");
-      JiraUserObject jira_llu2 = new JiraUserObject("llu-23", "Description for llu-2");
-
-      DefaultMutableTreeTableNode root = new DefaultMutableTreeTableNode("root");
-      DefaultMutableTreeTableNode sprintOne = addChildToParent(root, sprint_12_1);
-      DefaultMutableTreeTableNode sprintTwo = addChildToParent(root, sprint_12_2);
-      DefaultMutableTreeTableNode fixVersionOne = addChildToParent(sprintOne, fixVersion_11);
-      DefaultMutableTreeTableNode fixVersionTwo = addChildToParent(sprintOne, fixVersion_12);;
-      DefaultMutableTreeTableNode fixVersionTwoDupe = addChildToParent(sprintTwo, fixVersion_12);;
-      addChildToParent(fixVersionOne, jira_llu1);
-      addChildToParent(fixVersionTwo, jira_llu2);
-      addChildToParent(fixVersionTwoDupe, jira_llu2);
+      DefaultMutableTreeTableNode root = createTestRoot();
 
       DefaultTreeTableModel treeTableModel = new JiraTreeTableModel(root, BoardColumnMapperImpl.boardColumnMapping);
       Component treeTable = getTreeTable(treeTableModel);
@@ -81,6 +67,26 @@ public class TestTreeTable {
        * the latest state!
        */
 
+   }
+
+   private DefaultMutableTreeTableNode createTestRoot() {
+      SprintUserObject sprint_12_1 = new SprintUserObject("12-1");
+      SprintUserObject sprint_12_2 = new SprintUserObject("12-2");
+      FixVersionUserObject fixVersion_11 = new FixVersionUserObject("LLU 11");
+      FixVersionUserObject fixVersion_12 = new FixVersionUserObject("LLU 12");
+      JiraUserObject jira_llu1 = new JiraUserObject("llu-123", "Description for llu-1");
+      JiraUserObject jira_llu2 = new JiraUserObject("llu-23", "Description for llu-2");
+
+      DefaultMutableTreeTableNode root = new DefaultMutableTreeTableNode("root");
+      DefaultMutableTreeTableNode sprintOne = addChildToParent(root, sprint_12_1);
+      DefaultMutableTreeTableNode sprintTwo = addChildToParent(root, sprint_12_2);
+      DefaultMutableTreeTableNode fixVersionOne = addChildToParent(sprintOne, fixVersion_11);
+      DefaultMutableTreeTableNode fixVersionTwo = addChildToParent(sprintOne, fixVersion_12);;
+      DefaultMutableTreeTableNode fixVersionTwoDupe = addChildToParent(sprintTwo, fixVersion_12);;
+      addChildToParent(fixVersionOne, jira_llu1);
+      addChildToParent(fixVersionTwo, jira_llu2);
+      addChildToParent(fixVersionTwoDupe, jira_llu2);
+      return root;
    }
 
    private Component getTreeTable(DefaultTreeTableModel treeTableModel) {
