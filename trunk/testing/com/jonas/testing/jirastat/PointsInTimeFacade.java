8cf26015d0dfcfa713d480978bcc2f65aff61237
pjw.olofsson <pjw.olofsson@9ed4f39a-2752-0410-b9c2-9f4538f363dd>
2009-05-12 16:07:02 +0000

    Jonas: refactor

--- trunk/testing/com/jonas/testing/jirastat/PointsInTimeFacade.java
+++ trunk/testing/com/jonas/testing/jirastat/PointsInTimeFacade.java
@@ -1,40 +1,24 @@
 package com.jonas.testing.jirastat;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.jfree.data.time.Day;
 
-import com.jonas.jira.JiraStatus;
+public class PointsInTimeFacade<T> extends PointsInTimeFacadeAbstract<T>{
 
-public class PointsInTimeFacade {
-
-   private Map<TimeDTO, PointInTimeAgreggator> dataDaysAgreggators = new HashMap<TimeDTO, PointInTimeAgreggator>();
    private final boolean isWeek;
 
    public PointsInTimeFacade(boolean isWeek) {
       this.isWeek = isWeek;
    }
 
-   public List<PointInTimeAgreggator> getPointInTimes() {
-      List<PointInTimeAgreggator> list = new ArrayList<PointInTimeAgreggator>();
-      for(PointInTimeAgreggator aggregator : dataDaysAgreggators.values()){
-         list.add(aggregator);
-      }
-      return list;
-   }
-
-   public void addPointInTime(Day day, JiraStatus jiraStatus) {
+   public void addPointInTime(Day day, T barSeparator) {
       TimeDTO daydto = new TimeDTO(day, isWeek);
-      PointInTimeAgreggator pointInTimeAgreggator = dataDaysAgreggators.get(daydto);
+      PointInTimeAgreggator<T> pointInTimeAgreggator = dataDaysAgreggators.get(daydto);
       if (pointInTimeAgreggator == null) {
-         pointInTimeAgreggator = new PointInTimeAgreggator();
+         pointInTimeAgreggator = new PointInTimeAgreggator<T>();
          dataDaysAgreggators.put(daydto, pointInTimeAgreggator);
-         pointInTimeAgreggator.add(daydto.getDate(), jiraStatus);
+         pointInTimeAgreggator.add(daydto.getDate(), barSeparator);
       } else {
-         pointInTimeAgreggator.add(daydto.getDate(), jiraStatus);
+         pointInTimeAgreggator.add(daydto.getDate(), barSeparator);
       }
    }
 }
