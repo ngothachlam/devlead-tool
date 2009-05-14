11f66b904c149cb52ece22a3873debe81573babb
pjw.olofsson <pjw.olofsson@9ed4f39a-2752-0410-b9c2-9f4538f363dd>
2009-05-14 23:10:37 +0000

    Jonas: Refactored Manual Burndown

--- src/com/jonas/agile/devleadtool/gui/component/frame/AbstractBasicFrame.java
+++ src/com/jonas/agile/devleadtool/gui/component/frame/AbstractBasicFrame.java
@@ -2,7 +2,12 @@ package com.jonas.agile.devleadtool.gui.component.frame;
 
 import java.awt.Component;
 import java.awt.Container;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.JFrame;
+
+import com.jonas.agile.devleadtool.data.SystemProperties;
 import com.jonas.common.swing.SwingUtil;
 
 public abstract class AbstractBasicFrame extends JFrame {
@@ -11,13 +16,23 @@ public abstract class AbstractBasicFrame extends JFrame {
    private Integer width;
    private Integer height;
 
-   public AbstractBasicFrame(Integer width, Integer height) {
-      this.width = width;
-      this.height = height;
+   public AbstractBasicFrame(Component parent, Integer width, Integer height) {
+      this(parent, width, height, false);
    }
 
-   public AbstractBasicFrame(Component parent, Integer width, Integer height) {
-      this(width, height);
+   public AbstractBasicFrame(Component parent, Integer width, Integer height, boolean exitOnCLose) {
+      this.width = width;
+      this.height = height;
+      if (exitOnCLose) {
+         addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+               super.windowClosing(e);
+               SystemProperties.close();
+               System.exit(0);
+            }
+         });
+      }
       this.parent = parent;
    }
 
