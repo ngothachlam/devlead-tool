888fe5d61911613e2dbb5abcc40c9d56a1d39a3d
pjw.olofsson <pjw.olofsson@9ed4f39a-2752-0410-b9c2-9f4538f363dd>
2009-05-14 22:54:42 +0000

    Jonas: Refactored Manual Burndown

---
+++ src/com/jonas/agile/devleadtool/burndown/BurnDownData.java
@@ -0,0 +1,51 @@
+package com.jonas.agile.devleadtool.burndown;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class BurnDownData {
+
+   private Map<String, BurnDownData> burnDownDataPerCategory;
+   private List<BurnDownDay> burndownDays = new ArrayList<BurnDownDay>();
+
+   public BurnDownData(Map<String, BurnDownData> burnDownDataPerCategory) {
+      this.burnDownDataPerCategory = burnDownDataPerCategory;
+   }
+
+   public BurnDownData() {
+      this.burnDownDataPerCategory = new HashMap<String, BurnDownData>();
+   }
+
+   public void add(String category, double x, double value) {
+      BurnDownData data = getCategory(category);
+      if (data == null) {
+         data = new BurnDownData(burnDownDataPerCategory);
+         burnDownDataPerCategory.put(category, data);
+      }
+      data.add(new BurnDownDay(x, value));
+   }
+
+   private void add(BurnDownDay burnDownDay) {
+      burndownDays.add(burnDownDay);
+   }
+
+   private BurnDownData getCategory(String category) {
+      return burnDownDataPerCategory.get(category);
+   }
+
+   public List<BurnDownDay> getDataForCategory(String string) {
+      return getCategory(string).getData();
+   }
+
+   private List<BurnDownDay> getData() {
+      return burndownDays;
+   }
+
+   public Set<String> getCategoryNames() {
+      return burnDownDataPerCategory.keySet();
+   }
+
+}
--- src/com/jonas/agile/devleadtool/burndown/ManualBurnDownFrame.java
+++ src/com/jonas/agile/devleadtool/burndown/ManualBurnDownFrame.java
@@ -7,9 +7,7 @@ import java.awt.Frame;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
@@ -30,51 +28,31 @@ import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.chart.title.TextTitle;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
-import org.jfree.ui.RefineryUtilities;
 
 import com.jonas.agile.devleadtool.gui.action.BasicAbstractGUIAction;
-import com.jonas.agile.devleadtool.gui.burndown.CalculateSprintBurndownAction;
 import com.jonas.agile.devleadtool.gui.component.frame.AbstractBasicFrame;
-import com.jonas.agile.devleadtool.gui.component.table.MyTable;
-import com.jonas.agile.devleadtool.sprint.Sprint;
-import com.jonas.agile.devleadtool.sprint.SprintCache;
 import com.jonas.common.DateHelper;
 import com.jonas.common.string.StringHelper;
 import com.jonas.common.swing.SwingUtil;
 
-public class ManualBurnDownFrame extends AbstractBasicFrame  {
+public class ManualBurnDownFrame extends AbstractBasicFrame {
 
    private XYSeries idealProgression;
-   private XYSeries estimateProgression;
 
    private ValueAxis xAxis;
-   
+
    private ChartPanel panel;
    private DateHelper dateHelper;
    private TextTitle source;
    private NumberAxis yAxis;
    private JTextField name;
+   private XYSeriesCollection dataset;
 
-   public static void main(String[] args){
+   public static void main(String[] args) {
       ManualBurnDownFrame frame = new ManualBurnDownFrame(null, new DateHelper());
       frame.setVisible(true);
    }
-   private List<BurnDownDay> getBurndowns() {
-      List<BurnDownDay> burndownDays = new ArrayList<BurnDownDay>();
-      burndownDays.add(new BurnDownDay(0d,15d+7d));
-      burndownDays.add(new BurnDownDay(1d,16d+7d));
-      burndownDays.add(new BurnDownDay(2d,16d+5d));
-      burndownDays.add(new BurnDownDay(3d,13d+3d));
-      burndownDays.add(new BurnDownDay(4d,13d+1.5d));
-      burndownDays.add(new BurnDownDay(5d,13d+0d));
-      burndownDays.add(new BurnDownDay(6d,4d+7d));
-      burndownDays.add(new BurnDownDay(7d,2d+2d));
-      burndownDays.add(new BurnDownDay(8d,2d+4d));
-      burndownDays.add(new BurnDownDay(9d,1.75d+3d));
-      burndownDays.add(new BurnDownDay(10d,1.75d+2d));
-      return burndownDays;
-   }
-   
+
    public ManualBurnDownFrame(Component parent, DateHelper dateHelper) {
       super(parent, null, null);
       this.dateHelper = dateHelper;
@@ -84,27 +62,58 @@ public class ManualBurnDownFrame extends AbstractBasicFrame  {
    public void updateBurndown() {
       source.setText(name.getText());
       idealProgression.clear();
-      estimateProgression.clear();
-      
-      List<BurnDownDay> burndownDays = getBurndowns();
-      Collections.sort(burndownDays);
-      for (BurnDownDay burnDownDay : burndownDays) {
-         estimateProgression.add(burnDownDay.getX(), burnDownDay.getY());
+
+      BurnDownData data = getBurnDownData();
+
+      Set<String> categoryNames = data.getCategoryNames();
+      List<BurnDownDay> burndownDays = null;
+
+      double lengthOfSprint = 0d;
+      Double totalEstimate = 0d;
+      for (String categoryName : categoryNames) {
+         XYSeries estimateProgression = new XYSeries(categoryName);
+         burndownDays = data.getDataForCategory(categoryName);
+         estimateProgression.setKey(categoryName);
+
+         Collections.sort(burndownDays);
+         for (BurnDownDay burnDownDay : burndownDays) {
+            estimateProgression.add(burnDownDay.getX(), burnDownDay.getY());
+         }
+
+         lengthOfSprint = Math.max(lengthOfSprint, StringHelper.getDoubleOrZero(burndownDays.get(burndownDays.size() - 1).getX()));
+         totalEstimate = Math.max(totalEstimate, burndownDays.get(0).getY());
+
+         dataset.addSeries(estimateProgression);
       }
 
-      double lengthOfSprint = StringHelper.getDoubleOrZero(burndownDays.get(burndownDays.size()-1).getY());
-      
-      idealProgression.add(0, burndownDays.get(0).getY());
+      idealProgression.add(0, totalEstimate);
       idealProgression.add(lengthOfSprint, 0);
-      
+
+      xAxis.setAutoRange(true);
       xAxis.setLowerBound(0d);
-      xAxis.setUpperBound(Math.max(lengthOfSprint, 5) + 0.2d);
 
       yAxis.setAutoRange(true);
       yAxis.setLowerBound(0d);
 
    }
 
+   private BurnDownData getBurnDownData() {
+      BurnDownData data = new BurnDownData();
+
+      data.add("Real Progression", 0d, 15d + 7d);
+      data.add("Real Progression", 1d, 16d + 7d);
+      data.add("Real Progression", 2d, 16d + 5d);
+      data.add("Real Progression", 3d, 13d + 3d);
+      data.add("Real Progression", 4d, 13d + 1.5d);
+      data.add("Real Progression", 5d, 13d + 0d);
+      data.add("Real Progression", 6d, 4d + 7d);
+      data.add("Real Progression", 7d, 2d + 2d);
+      data.add("Real Progression", 8d, 2d + 4d);
+      data.add("Real Progression", 9d, 1.75d + 3d);
+      data.add("Real Progression", 10d, 1.75d + 2d);
+
+      return data;
+   }
 
    @Override
    public Container getMyPanel() {
@@ -141,11 +150,9 @@ public class ManualBurnDownFrame extends AbstractBasicFrame  {
    }
 
    public void prepareBurndown() {
-      estimateProgression = new XYSeries("Real Progression");
       idealProgression = new XYSeries("Ideal Progression");
 
-      XYSeriesCollection dataset = new XYSeriesCollection();
-      dataset.addSeries(estimateProgression);
+      dataset = new XYSeriesCollection();
       dataset.addSeries(idealProgression);
 
       // create the chart...
@@ -182,8 +189,8 @@ public class ManualBurnDownFrame extends AbstractBasicFrame  {
 
       panel = new ChartPanel(chart);
    }
-   
-   private class UpdateAction extends BasicAbstractGUIAction{
+
+   private class UpdateAction extends BasicAbstractGUIAction {
 
       private final ManualBurnDownFrame frame;
 
@@ -196,35 +203,7 @@ public class ManualBurnDownFrame extends AbstractBasicFrame  {
       public void doActionPerformed(ActionEvent e) {
          frame.updateBurndown();
       }
-      
+
    }
-   
-   private class BurnDownDay implements Comparable<BurnDownDay>{
 
-      private Double x, y;
-      
-
-      public BurnDownDay(Double x, Double y) {
-         this.x = x;
-         this.y = y;
-      }
-
-      public Double getY() {
-         return y;
-      }
-
-      public Double getX() {
-         return x;
-      }
-
-      @Override
-      public int compareTo(BurnDownDay o) {
-         Double y2 = o.getX();
-         Double y3 = getX();
-         return (int) (y3.doubleValue()-y2.doubleValue());
-      }
-      
-   }
 }
-
-
