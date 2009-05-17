d0f202ac5ba331e8d427429fb308f39e54c8f16e
pjw.olofsson <pjw.olofsson@9ed4f39a-2752-0410-b9c2-9f4538f363dd>
2009-05-17 20:28:46 +0000

    Working on BurnUp...

--- trunk/src/com/jonas/agile/devleadtool/burndown/ManualBurnDownFrame.java
+++ trunk/src/com/jonas/agile/devleadtool/burndown/ManualBurnDownFrame.java
@@ -2,14 +2,7 @@ package com.jonas.agile.devleadtool.burndown;
 
 import java.awt.Component;
 
-import org.jfree.chart.renderer.AbstractRenderer;
-import org.jfree.chart.renderer.xy.XYItemRenderer;
-import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
-import org.jfree.data.xy.XYDataset;
-import org.jfree.data.xy.XYSeriesCollection;
-
 import com.jonas.common.DateHelper;
-import com.jonas.common.swing.SwingUtil;
 
 public class ManualBurnDownFrame extends AbstractManualBurnFrame {
 
@@ -20,23 +13,29 @@ public class ManualBurnDownFrame extends AbstractManualBurnFrame {
 
          @Override
          public void calculateBurndownData() {
-            data = new BurnDataCategory();
-            CategoryType string = new CategoryType("Real Progression",BurnType.BurnDown);
-            data.add(string, 0d, 15d + 7d);
-            data.add(string, 1d, 16d + 7d);
-            data.add(string, 2d, 16d + 5d);
-            data.add(string, 3d, 13d + 3d);
-            data.add(string, 4d, 13d + 1.5d);
-            data.add(string, 5d, 13d + 0d);
-            data.add(string, 6d, 4d + 7d);
-            data.add(string, 7d, 2d + 2d);
-            data.add(string, 8d, 2d + 4d);
-            data.add(string, 9d, 1.75d + 3d);
-            data.add(string, 10d, 1.75d + 2d);
+            data = new BurnDataCategory(BurnType.BurnDown);
+            
+            Category category1 = new Category("Real Progression");
+            double total = 15d + 7d;
+            double length = 10d;
+            data.add(category1, 0d, total);
+            data.add(category1, 1d, 16d + 7d);
+            data.add(category1, 2d, 16d + 5d);
+            data.add(category1, 3d, 13d + 3d);
+            data.add(category1, 4d, 13d + 1.5d);
+            data.add(category1, 5d, 13d + 0d);
+            data.add(category1, 6d, 4d + 7d);
+            data.add(category1, 7d, 2d + 2d);
+            data.add(category1, 8d, 2d + 4d);
+            data.add(category1, 9d, 1.75d + 3d);
+            data.add(category1, length, 1.75d + 2d);
 
-            CategoryType string2 = new CategoryType("Ideal Progression",BurnType.BurnDown);
-            data.add(string2, 0d, 15d + 7d);
-            data.add(string2, 10d, 0d);
+            Category category2 = new Category("Ideal Progression");
+            double decrease = total/length;
+            for (int xValue = 0; xValue <= length; xValue++) {
+               data.add(category2, xValue, total);
+               total -= decrease;
+            }
          }
 
          @Override
@@ -55,22 +54,4 @@ public class ManualBurnDownFrame extends AbstractManualBurnFrame {
       super(parent, dateHelper, retriever, closeOnExit);
    }
 
-   @Override
-   public XYItemRenderer getRenderer() {
-      return new XYLineAndShapeRenderer(true, false);
-   }
-
-   @Override
-   public void setRendererPaints(AbstractRenderer renderer) {
-      int row = 0;
-      renderer.setSeriesPaint(row++, SwingUtil.cellBlue);
-      renderer.setSeriesPaint(row++, SwingUtil.cellLightBlue);
-      renderer.setSeriesPaint(row++, SwingUtil.cellRed);
-      renderer.setSeriesPaint(row++, SwingUtil.cellLightRed);
-      renderer.setSeriesPaint(row++, SwingUtil.cellGreen);
-      renderer.setSeriesPaint(row++, SwingUtil.cellLightGreen);
-      renderer.setSeriesPaint(row++, SwingUtil.cellLightYellow);
-      ((XYLineAndShapeRenderer) renderer).setShapesVisible(true);
-      ((XYLineAndShapeRenderer) renderer).setShapesFilled(true);
-   }
 }
--- trunk/src/com/jonas/agile/devleadtool/burndown/ManualBurnUpFrame.java
+++ trunk/src/com/jonas/agile/devleadtool/burndown/ManualBurnUpFrame.java
@@ -2,14 +2,7 @@ package com.jonas.agile.devleadtool.burndown;
 
 import java.awt.Component;
 
-import org.jfree.chart.renderer.AbstractRenderer;
-import org.jfree.chart.renderer.xy.StackedXYAreaRenderer2;
-import org.jfree.chart.renderer.xy.XYItemRenderer;
-import org.jfree.data.xy.DefaultTableXYDataset;
-import org.jfree.data.xy.XYDataset;
-
 import com.jonas.common.DateHelper;
-import com.jonas.common.swing.SwingUtil;
 
 public class ManualBurnUpFrame extends AbstractManualBurnFrame {
 
@@ -20,33 +13,34 @@ public class ManualBurnUpFrame extends AbstractManualBurnFrame {
 
          @Override
          public void calculateBurndownData() {
-            data = new BurnDataCategory();
-            CategoryType category = new CategoryType("Closed",BurnType.BurnUp);
+            data = new BurnDataCategory(BurnType.BurnUp);
+            
+            Category category = new Category("Closed");
             data.add(category, 0d, 0d);
             data.add(category, 1d, 1d);
             data.add(category, 2d, 2d);
 
-            CategoryType category2 = new CategoryType("Resolved",BurnType.BurnUp);
+            Category category2 = new Category("Resolved");
             data.add(category2, 0d, 0d);
             data.add(category2, 1d, 1d);
             data.add(category2, 2d, 2d);
 
-            CategoryType category3 = new CategoryType("In-Progress",BurnType.BurnUp);
+            Category category3 = new Category("In-Progress");
             data.add(category3, 0d, 0d);
             data.add(category3, 1d, 1d);
             data.add(category3, 2d, 1d);
 
-            CategoryType category4 = new CategoryType("Failed",BurnType.BurnUp);
+            Category category4 = new Category("Failed");
             data.add(category4, 0d, 0d);
             data.add(category4, 1d, 1d);
             data.add(category4, 2d, 0d);
 
-            CategoryType category5 = new CategoryType("Open",BurnType.BurnUp);
+            Category category5 = new Category("Open");
             data.add(category5, 0d, 3d);
             data.add(category5, 1d, 2d);
             data.add(category5, 2d, 1d);
 
-            CategoryType category6 = new CategoryType("Datafixes completed",BurnType.BurnUp);
+            Category category6 = new Category("Datafixes completed");
             double dataFixes = 0d;
             data.add(category6, 0d, dataFixes += 2d);
             data.add(category6, 1d, dataFixes += 1d);
@@ -67,24 +61,5 @@ public class ManualBurnUpFrame extends AbstractManualBurnFrame {
 
    public ManualBurnUpFrame(Component parent, DateHelper dateHelper, BurnDataRetriever retriever, boolean closeOnExit) {
       super(parent, dateHelper, retriever, closeOnExit);
-   }
-
-
-   @Override
-   public XYItemRenderer getRenderer() {
-      return new StackedXYAreaRenderer2();
-   }
-
-   @Override
-   public void setRendererPaints(AbstractRenderer renderer) {
-      int row = 0;
-      renderer.setSeriesPaint(row++, SwingUtil.cellGreen);
-      renderer.setSeriesPaint(row++, SwingUtil.cellBlue);
-      renderer.setSeriesPaint(row++, SwingUtil.cellLightBlue);
-      renderer.setSeriesPaint(row++, SwingUtil.cellLightRed);
-      renderer.setSeriesPaint(row++, SwingUtil.cellWhite);
-      renderer.setSeriesPaint(row++, SwingUtil.cellLightYellow);
-      renderer.setSeriesPaint(row++, SwingUtil.cellLightGreen);
-      renderer.setSeriesPaint(row++, SwingUtil.cellLightYellow);
    }
 }
