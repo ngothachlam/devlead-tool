7b48a0cc8545a1dcb676b470da6d0107733e9803
pjw.olofsson <pjw.olofsson@9ed4f39a-2752-0410-b9c2-9f4538f363dd>
2009-06-08 14:32:40 +0000

    Jonas: Working on JiraStat Charts

---
+++ trunk/testing/com/jonas/stats/charts/db/DBDao.java
@@ -0,0 +1,16 @@
+package com.jonas.stats.charts.db;
+
+import java.io.File;
+import java.io.IOException;
+import com.jonas.agile.devleadtool.burndown.ContentsDto;
+import com.jonas.stats.charts.excel.Dao;
+
+public class DBDao implements Dao{
+
+   @Override
+   public ContentsDto loadContents(File xlsFile, String excelSheet) throws IOException {
+      // TODO Auto-generated method stub
+      throw new RuntimeException("Method not implemented yet!");
+   }
+
+}
